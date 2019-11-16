ac6eb9d9f8a7b12574262351a5c2ab17410e8637
Filip <sotiroski_filip@hotmail.com>
2019-11-16 16:44:58 +0100

    alignement new version

--- question4.py
+++ question4.py
@@ -1,15 +1,11 @@
 import itertools
 
-x = "ctg"
-k_x = 2
-y = "tc"
-k_y = ""
-x_gap = x
-y_gap = ""
+
 
 def nb_alignements_possibles(x,y,k_x):
     """
     str, str, int -> int
+    retourne le nombre des facons pour inserer les gaps dans y
     """
     if(len(x) < len(y)):
         raise Exception("Il faut que n>=m!")
@@ -24,32 +20,53 @@ def nb_alignements_possibles(x,y,k_x):
     
     #cration de y_gap
     y_gap = creation_gap(y, k_y)
-    print(x_gap)
-    print(y_gap)
+    print("X_gap: {}".format(x_gap))
+    print("Y_gap: {}\n".format(y_gap))
 
     #creation de tous les mots de x_gap
-    mots_de_x_gap = list(set(["".join(perm) for perm in itertools.permutations(x_gap)]))
-    print(mots_de_x_gap)
-    
-    #creation de tous les mots de y_gap
-    mots_de_y_gap = list(set(["".join(perm) for perm in itertools.permutations(y_gap)]))
-    print(mots_de_y_gap)
+    mots_de_x_gap = []
 
+    wordlength = len(x) + k_x
+    characters = x
+    for indices in itertools.combinations(range(wordlength), len(characters)):
+        s = ['-']*wordlength
+        for index, character in zip(indices, characters):
+            s[index] = character
+        mots_de_x_gap.append(''.join(s))
+    
+    print("Tous les mots de X_gap: {}\n{}".format(len(mots_de_x_gap),mots_de_x_gap))
+
+    #creation de tous les mots de y_gap
+    mots_de_y_gap = []
+    wordlength = len(y) + k_y
+    characters = y
+    for indices in itertools.combinations(range(wordlength), len(characters)):
+        s = ['-']*wordlength
+        for index, character in zip(indices, characters):
+            s[index] = character
+        mots_de_y_gap.append(''.join(s))
+
+    #print("Tous les mots de Y_gap: {}\n{}".format(len(mots_de_y_gap),mots_de_y_gap))
+
+    print("Mots y gap:", len(mots_de_y_gap))
+    
     y_aligne = []
     #eviter les gaps doublant
     for motX in mots_de_x_gap:
         for motY in mots_de_y_gap:
+            ajouter = True
             for i in range(len(motX)):
                 if(motX[i]=="-" and motY[i]=="-"):
+                    ajouter = False
                     break
-            if(motY not in y_aligne):
+            if(motY not in y_aligne and ajouter == True):
                 y_aligne.append(motY)
-    print(y_aligne)            
+           
+    #print("Total alignements de Y_gap: {}".format(len(y_aligne)))
     print(len(y_aligne))
-
-
-
-
+    #print(y_aligne)     
+    
+    return len(y_aligne)
 
 
 def creation_gap(mot, k):
@@ -58,4 +75,12 @@ def creation_gap(mot, k):
         mot_gap = mot_gap +"-"
     return mot_gap
 
+x = "ctfctfctfctfctf"
+k_x = 1
+y = "gtcgtcgtcg"
+# k_y = ""
+# x_gap = x
+# y_gap = ""
+
 nb_alignements_possibles(x,y,k_x)
+
