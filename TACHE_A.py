ccdbb6bd4990c61f082507f27d050a4973b2c683
SOTIROSKI <sotiroski_filip@hotmail.com>
2019-12-04 11:53:21 +0100

    temp

--- TACHE_A.py
+++ TACHE_A.py
@@ -31,27 +31,23 @@ def DIST_NAIF_REC(x,y,i,j,c,dist):
 
     return dist    
 
-x,y = readFile("./Inst_0000010_44.adn")
-print(DIST_NAIF(x,y)) #10
-x,y = readFile("./Inst_0000010_7.adn")
-print(DIST_NAIF(x,y)) #8
-x,y = readFile("./Inst_0000010_8.adn")
-print(DIST_NAIF(x,y)) #2
-
+# x,y = readFile("./Instances_genome/Inst_0000010_44.adn")
+# print(DIST_NAIF(x,y)) #10
 # x,y = readFile("./Instances_genome/Inst_0000010_7.adn")
-# # print(DIST_NAIF(x,y)) #2
-# start_time = time.time()
-# DIST_NAIF(x,y)
-# print((time.time() - start_time))
+# print(DIST_NAIF(x,y)) #8
+# x,y = readFile("./Instances_genome/Inst_0000010_8.adn")
+# print(DIST_NAIF(x,y)) #2
 
-# listFiles = ["Inst_0000010_8", "Inst_0000012_56","Inst_0000013_45","Inst_0000014_7","Inst_0000015_2","Inst_0000020_8","Inst_0000050_3"]
 
-# for ls in listFiles:
-#     x,y = readFile("./Instances_genome/"+ls+".adn")
-#     n=len(x)
-#     m=len(y)
-#     start_time = time.time()
-#     DIST_NAIF(x,y)
-#     print("{}:{}".format(n, (time.time() - start_time)))
+
+listFiles = ["Inst_0000010_8", "Inst_0000012_56","Inst_0000013_45","Inst_0000014_7","Inst_0000015_2","Inst_0000020_8","Inst_0000050_3"]
+
+for ls in listFiles:
+    x,y = readFile("./Instances_genome/"+ls+".adn")
+    n=len(x)
+    m=len(y)
+    start_time = time.time()
+    DIST_NAIF(x,y)
+    print("{}:{}".format(n, (time.time() - start_time)))
 
 
